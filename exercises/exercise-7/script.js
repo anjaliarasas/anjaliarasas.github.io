f71586e8df3aa34ff7ceaae5b8edc0374a780fcc
anjaliarasas <113141493+anjaliarasas@users.noreply.github.com>
2023-03-05 17:13:24 -0500

    Exercise 7

---
+++ exercises/exercise-7/script.js
@@ -0,0 +1,20 @@
+const btn = document.querySelector("button");
+if (btn) {
+  btn.onclick = function () {
+ 
+    btn.classList.toggle("dipped");
+  };
+}
+
+
+
+let goto = (file, line) => {
+  window.parent.postMessage(
+    { type: "glitch/go-to-line", payload: { filePath: file, line: line } }, "*"
+  );
+};
+
+const filer = document.querySelectorAll(".fileopener");
+filer.forEach((f) => {
+  f.onclick = () => { goto(f.dataset.file, f.dataset.line); };
+});
