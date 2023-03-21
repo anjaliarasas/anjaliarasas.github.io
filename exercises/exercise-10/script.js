02a509be3cfffaab0fbe876eadd0229b21337c52
anjaliarasas <113141493+anjaliarasas@users.noreply.github.com>
2023-03-21 11:35:17 -0400

    ...

---
+++ exercises/exercise-10/script.js
@@ -0,0 +1,11 @@
+let color = "WOOF!!"
+  console.log(speak)
+
+let char = speak[3];
+  console.log(speak.length)
+  console.log(char)
+
+for(let i = 0; i < 10; i++) {
+  console.log("i love bread")
+  console.log(i)
+}
