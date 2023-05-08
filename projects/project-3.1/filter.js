0e57f553810cd24e27cdc251e194454ac46a29ce
anjaliarasas <113141493+anjaliarasas@users.noreply.github.com>
2023-05-08 18:52:00 -0400

    hi

--- projects/project-3.1/filter.js
+++ projects/project-3.1/filter.js
@@ -122,20 +122,6 @@ const renderItems = (collection) => {
     })
 }
 
-var images = ["assets/library1.jpg","assets/library2.jpg","assets/library3.jpgassets/library4.jpg","assets/library5.jpg","assets/library6.jpgassets/library7.jpg","assets/library.jpg","assets/library6.jpg"];
-var i = 0;
-var renew = setInterval(function(){
-    if(links.length == i){
-        i = 0;
-    }
-    else {
-    document.getElementById("bannerImage").src = images[i]; 
-    document.getElementById("bannerLink").href = links[i]; 
-    i++;
-
-}
-},30000);
-
 //function changeColor(id)document.getElementById("closed").addEventListener("click", function() {alert("Hello World!");});
 //
 
